c5f9a3c3dc0c4fa37a99ca0a3da2685e3d93f877
joker-termux <68482235+joker-termux@users.noreply.github.com>
2020-07-19 00:12:52 +0200

    Update joker-facebook-hacker.py

--- joker-facebook-hacker.py
+++ joker-facebook-hacker.py
@@ -1,14 +1,14 @@
 from os import *
 from sys import *
 from time import *
-###                 colors                  R='\033[1;31m'                              G='\033[1;32m'
-Y='\033[1;33m'
-C='\033[1;36m'                              W='\033[1;37m'
+###                 colors                  R='\033[1;31m'          W='\033[1;37m'         Y='\033[1;33m'     C='\033[1;36m'      G='\033[1;32m'
+
+                              
 #12###              banner
 
 system('clear')
 system('toilet -f mono12 -F gay joker')
-print("\033[1;32m       ShadoW--{ ^_^ }--Welcome To Tool Joker ")
+print("\033[1;32m       *Formatter for your device by eljoker* ")
 print("\033[1;37m       ====================================== ")
 print(" ")
 
